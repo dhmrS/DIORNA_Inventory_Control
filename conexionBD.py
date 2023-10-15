637578611cb013227ce707f2ec65bdd0471c6008
Diego Mendoza <143966168+dhmrS@users.noreply.github.com>
2023-10-14 17:28:53 -0700

    Update conexionBD.py

--- conexionBD.py
+++ conexionBD.py
@@ -1,45 +1,58 @@
 import sqlite3
 
+
 class Registro_datos():
     def __init__(self):
-        self.conexion = sqlite3.connect("inventory_data_base.db") # Conexión a DB
-    
-    def inserta_producto(self,codigo, nombre, modelo, precio, cantidad):
-        cur = self.conexion.cursor()
-        sql='''INSERT INTO productos (CODIGO, NOMBRE, MODELO, PRECIO, CANTIDAD) 
-        VALUES('{}', '{}','{}', '{}','{}')'''.format(codigo, nombre, modelo, precio, cantidad)
-        cur.execute(sql)
-        self.conexion.commit()
-        cur.close()
+        self.conexion = sqlite3.connect(
+            "DB.db")  # Conexión a DB
+    #-----------------------------------------------------------------------------------------------
     def buscar_productos(self):
         cursor = self.conexion.cursor()
-        sql = "SELECT * FROM productos " 
+        sql = "SELECT * FROM productos "
         cursor.execute(sql)
         registro = cursor.fetchall()
         return registro
-
+    #----------------------------------------------------------------------------------------------
     def busca_producto(self, nombre_producto):
         cur = self.conexion.cursor()
         sql = "SELECT * FROM productos WHERE NOMBRE = {}".format(nombre_producto)
         cur.execute(sql)
         nombrex = cur.fetchall()
-        cur.close()     
+        cur.close()
         return nombrex
-
-    def elimina_productos(self,nombre):
+    #-----------------------------------------------------------------------------------------------
+    def inserta_producto(self, codigo, nombre, modelo, precio, cantidad):
         cur = self.conexion.cursor()
-        sql='''DELETE FROM productos WHERE NOMBRE = {}'''.format(nombre)
+        sql = '''INSERT INTO productos (CODIGO, NOMBRE, MODELO, PRECIO, CANTIDAD) 
+        VALUES('{}', '{}','{}', '{}','{}')'''.format(codigo, nombre, modelo, precio, cantidad)
+        cur.execute(sql)
+        self.conexion.commit()
+        cur.close()
+    #-----------------------------------------------------------------------------------------------
+    def actualiza_productos(self, codigo, nombre, modelo, precio, cantidad, codigo_anterior):
+        cur = self.conexion.cursor()
+        sql = '''UPDATE productos SET
+            CODIGO = ?, NOMBRE = ?, MODELO = ?, PRECIO = ?, CANTIDAD = ? WHERE CODIGO = ?'''
+        valores = (codigo, nombre, modelo, precio, cantidad, codigo_anterior)
+        cur.execute(sql, valores)
+        act = cur.rowcount
+        self.conexion.commit()
+        cur.close()
+        return act
+    #-----------------------------------------------------------------------------------------------
+    def elimina_producto(self, codigo):
+        cur = self.conexion.cursor()
+        sql = '''DELETE FROM productos WHERE CODIGO = {}'''.format(codigo)
         cur.execute(sql)
         nom = cur.rowcount
-        self.conexion.commit()    
+        self.conexion.commit()
         cur.close()
-        return nom   
-    def actualiza_productos(self, codigo, nombre, modelo, precio, cantidad):
+        return nom
+
+    def buscar_producto_para_modificar(self, codigo_producto):
         cur = self.conexion.cursor()
-        sql ='''UPDATE productos SET  CODIGO =' {}' , MODELO = '{}', PRECIO = '{}', CANTIDAD = '{}'
-        WHERE NOMBRE = '{}' '''.format(codigo,  modelo, precio, cantidad, nombre)
+        sql = "SELECT * FROM productos WHERE CODIGO = {}".format(codigo_producto)
         cur.execute(sql)
-        act = cur.rowcount
-        self.conexion.commit()    
+        nombrex = cur.fetchall()
         cur.close()
-        return act  
+        return nombrex
